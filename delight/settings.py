9dc749d8ed043374349b46487b14fcf763d7473d
Faysal <Faysalgeesle@hotmail.com>
2024-06-14 18:09:48 +0000

    Add necessary files and settings for deployment

--- delight/settings.py
+++ delight/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-faysalahmed-delightrest-9orh63mjs7r.ws-eu114.gitpod.io' , '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-faysalahmed-delightrest-9orh63mjs7r.ws-eu114.gitpod.io' , 'delight-restaurant-3f0223201abb.herokuapp.com']
 
 
 # Application definition
