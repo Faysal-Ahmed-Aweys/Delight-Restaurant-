d8acefd5a501994e76b728fe2aba69efd4317e6a
Faysal <Faysalgeesle@hotmail.com>
2024-06-20 16:03:43 +0000

    Create update-user form

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,3 +17,24 @@ class CustomLoginForm(LoginForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['login'].label = 'Email / Username'
+
+class UpdateUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+    
+    def __init__(self, *args, **kwargs):
+        super(UpdateUserForm, self).__init__(*args, **kwargs)
+        self.user_id = kwargs['instance'].id
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username').lower()
+        if User.objects.filter(username__iexact=username).exclude(id=self.user_id).exists():
+            raise forms.ValidationError("This username is already taken. Please choose a different one.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email').lower()
+        if User.objects.filter(email__iexact=email).exclude(id=self.user_id).exists():
+            raise forms.ValidationError("This email is already registered. Please choose a different one.")
+        return email
