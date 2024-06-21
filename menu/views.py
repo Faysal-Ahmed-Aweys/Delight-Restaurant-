060ab92aa3f34ec7541aa623ffb2d1dc10562659
Faysal <Faysalgeesle@hotmail.com>
2024-06-21 16:44:07 +0000

    Add menu page template, view and url

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import MenuItem
+from itertools import chain
 
-# Create your views here.
+def menu(request):
+    starters = MenuItem.objects.filter(category='starter')
+    mains = MenuItem.objects.filter(category='main')
+    desserts = MenuItem.objects.filter(category='dessert')
+
+    combined_menu = list(chain(starters, mains, desserts))
+
+    return render(request, 'menu.html', {
+        'starters': starters,
+        'mains': mains,
+        'desserts': desserts,
+        'combined_menu': combined_menu,
+    })
