080cbd9c27afdfd532153b59aa6936438e6de010
Faysal <Faysalgeesle@hotmail.com>
2024-06-25 15:00:34 +0000

    Add docstrings to forms in accounts app

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 from allauth.account.forms import SignupForm, LoginForm
 
 class CustomSignupForm(SignupForm):
+    """
+    Custom signup form extending Allauth's SignupForm to include first_name and last_name fields.
+
+    Attributes:
+        first_name (forms.CharField): Field for user's first name.
+        last_name (forms.CharField): Field for user's last name.
+    """
+
     first_name = forms.CharField(max_length=30, label='First Name', required=True)
     last_name = forms.CharField(max_length=30, label='Last Name', required=True)
 
@@ -13,12 +21,30 @@ class CustomSignupForm(SignupForm):
         user.save()
         return user
 
+
 class CustomLoginForm(LoginForm):
+    """
+    Custom login form extending Allauth's LoginForm to customize the login field label.
+
+    Methods:
+        __init__(*args, **kwargs): Initializes the form and customizes the 'login' field label.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['login'].label = 'Email / Username'
 
 class UpdateUserForm(forms.ModelForm):
+    """
+    Form for updating user information.
+
+    Inherits from Django's ModelForm for the User model and adds custom validation for unique usernames and emails.
+
+    Methods:
+        clean_username(): Custom validation to ensure username uniqueness.
+        clean_email(): Custom validation to ensure email uniqueness.
+    """
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
